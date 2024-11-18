f6f9355d918985019ee9a159f47dda7719c23307
pruffit <kotlaevdanil@gmail.com>
2024-11-19 03:21:00 +0400

    chor: add prettier, shadcn/ui

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from '@/components/ui/button'
+
+export default function Home() {
+  return (
+  <main className="p-24 flex flex-col row-start-2 items-center">
+    <Button>Click me</Button>
+  </main>
+  );
+}
