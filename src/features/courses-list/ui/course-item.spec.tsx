b67966fcf6e10da28b981aa7b1c00cfd0b7d98e4
pruffit <kotlaevdanil@gmail.com>
2024-11-23 09:34:30 +0400

    test

--- src/features/courses-list/ui/course-item.spec.tsx
+++ src/features/courses-list/ui/course-item.spec.tsx
@@ -1,18 +1,23 @@
-import { render, screen } from '@testing-library/react'
-import { CourseItem } from './course-item'
-import userEvent from '@testing-library/user-event'
+import { render, screen } from "@testing-library/react";
+import { CourseItem } from "./course-item";
+import userEvent from "@testing-library/user-event";
 
-describe('CourseItem', () => {
-	it('should call delete callback', async () => {
-		const onDelete = jest.fn()
-		render(<CourseItem course={{
-			id: '1',
-			name: 'Test',
-			description: 'Test',
-		}} onDelete={onDelete} />)
+describe("CourseItem", () => {
+  it("should call delete callback", async () => {
+    const onDelete = jest.fn();
+    render(
+      <CourseItem
+        course={{
+          id: "1",
+          name: "Test",
+          description: "Test",
+        }}
+        onDelete={onDelete}
+      />,
+    );
 
-		await userEvent.click(screen.getByText('Delete'))
+    await userEvent.click(screen.getByText("Delete"));
 
-		expect(onDelete).toHaveBeenCalled()
-	})
-})
+    expect(onDelete).toHaveBeenCalled();
+  });
+});
--- tests/courses-list.spec.ts
+++ tests/courses-list.spec.ts
@@ -1,17 +1,15 @@
 import { test, expect } from "@playwright/test";
 
-test('create delete course list', async ({ page }) => {
+test("create delete course list", async ({ page }) => {
   await page.goto("/");
-  await page.getByPlaceholder('название').click();
-  await page.getByPlaceholder('название').fill('Test');
-  await page.getByPlaceholder('описание').click();
-  await page.getByPlaceholder('описание').fill('Test');
-  await page.getByRole('button', { name: 'Добавить' }).click();
-  await expect(page.getByText('TestTestDelete')).toBeVisible();
-  await page.getByRole('button', { name: 'Delete' }).click();
-  await expect(page.getByText('CoursesНазвание ОписаниеTest')).toBeVisible();
+  await page.getByPlaceholder("название").click();
+  await page.getByPlaceholder("название").fill("Test");
+  await page.getByPlaceholder("описание").click();
+  await page.getByPlaceholder("описание").fill("Test");
+  await page.getByRole("button", { name: "Добавить" }).click();
+  await expect(page.getByText("TestTestDelete")).toBeVisible();
+  await page.getByRole("button", { name: "Delete" }).click();
+  await expect(page.getByText("CoursesНазвание ОписаниеTest")).toBeVisible();
 
-	await expect(
-    page.getByText("TestTestDelete"),
-  ).not.toBeVisible();
+  await expect(page.getByText("TestTestDelete")).not.toBeVisible();
 });
